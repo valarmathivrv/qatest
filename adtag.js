5bdab4a039ca18c302fd4def90fdd7beadfe638f
Valarmathi A <valarmathi@vervemobile.com>
2019-08-13 16:38:13 +0530

    Update adtag.js

--- adtag.js
+++ adtag.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');  
-script.src = "ttps://raw.githack.com/valarmathivrv/qatest/master/adtag.js";  
+script.src = "https://raw.githack.com/valarmathivrv/qatest/master/adtag.js";  
 document.head.appendChild(script);
